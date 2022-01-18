0a584d01650cd61362ffd02e267d2e0487386dc6
antropushka <97852620+antropushka@users.noreply.github.com>
2022-01-18 17:09:50 +0300

    Add files via upload

--- DooBee.java
+++ DooBee.java
@@ -1,14 +1,13 @@
 public class DooBee {
-public static void main (String[] args) {
-int x=1;
-while (x<3){
-System.out.print ("Doo");
-System.out.print ("Bee");
-x=x+1;
+    public static void main (String[] args) {
+        int x=1;
+        while (x<3){
+            System.out.print ("Doo");
+            System.out.print ("Bee");
+            x=x+1;
+        }
+        if (x==3){
+            System.out.print ("Do");
+        }
+    }
 }
-if (x==3){
-System.out.print ("Do");
-}
-}
-}
-
